896e0b52b18be93da56c32b308b976209ec4acf8
Jaap Aarts <jaap.aarts1@gmail.com>
2024-03-13 00:30:25 +0100

    add files not committed before

---
+++ go/cmd/create_trec/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"faissextcode"
+	"fmt"
+	"os"
+
+	_ "github.com/marcboeker/go-duckdb"
+)
+
+func main() {
+	db, err := faissextcode.SetupDatabase()
+	if err != nil {
+		fmt.Println("Unable to setup database:", err)
+		return
+	}
+
+	_, err = db.Exec("CALL FAISS_LOAD('flat', 'conformanceTests/index_IVF2048')")
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = db.Exec("CREATE TABLE queries AS SELECT qid, vector AS embedding FROM 'conformanceTests/anserini-tools/topics-and-qrels/topics.dl19-passage.openai-ada2.jsonl.gz'")
+	if err != nil {
+		panic(err)
+	}
+	rows, err := db.Query("SELECT qid, UNNEST(faiss_search('flat', 1000, embedding)) FROM queries")
+	if err != nil {
+		panic(err)
+	}
+
+	file, err := os.Create("results")
+	if err != nil {
+		panic(err)
+	}
+	var id int
+	var x map[string]interface{}
+	for rows.Next() {
+		rows.Scan(&id, &x)
+		if x["label"].(int64) != -1 {
+			fmt.Fprintln(file, id, 0, x["label"], x["distance"], x["rank"], "TODO")
+		}
+	}
+}
