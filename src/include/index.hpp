50c73e5d1cd37033e9a7ae9d7f2c26556e79202c
Jaap Aarts <jaap.aarts1@gmail.com>
2026-03-09 12:49:28 +0100

    Add support for duckdb 1.4 and 1.5

--- src/include/index.hpp
+++ src/include/index.hpp
@@ -53,10 +53,14 @@ struct FaissIndexEntry : ObjectCacheEntry {
 	string GetObjectType() override {
 		return FaissIndexEntry::ObjectType();
 	}
-
+#ifndef DUCKDB_MAJOR_VERSION
+#error "NO MAJOR VERSION"
+#endif
+#if DUCKDB_MAJOR_VERSION >= 2 || DUCKDB_MINOR_VERSION >= 5
 	optional_idx GetEstimatedCacheMemory() const override {
-		// Per the docs of 1.5 returning invalid index (presumably negative) should prevent eviction
+		// Per the docs of 1.5 returning invalid index should prevent eviction
 		return optional_idx {};
 	}
+#endif
 };
 } // namespace duckdb
